f43593616705dc68366c32912b369a32d3058f96
chendm <2283973011@qq.com>
2019-03-15 18:51:30 +0800

    add some variable and reset css

---
+++ src/components/ModuleList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="root-module-list">
+    <ul>
+      <li>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModuleList'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
